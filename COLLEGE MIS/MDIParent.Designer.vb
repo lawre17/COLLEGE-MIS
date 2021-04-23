2e371e9daf4132bc0d0e1f13f78954a9aa9e5477
NyagaBernard <83066270+NyagaBernard@users.noreply.github.com>
2021-04-23 13:36:11 +0300

    Add files via upload

---
+++ COLLEGE MIS/MDIParent.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MDIParent
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ChangePasswordToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LogOutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EnquiryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CustomerCareToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RecordsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RecordsToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CourseListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StudentsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FinanceToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UtilitiesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AdminstrationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RegistrationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GradesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LevelsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExamBodiesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CoursesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClassesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UsersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SettingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EnquiryToolStripMenuItem, Me.RecordsToolStripMenuItem1, Me.FinanceToolStripMenuItem, Me.UtilitiesToolStripMenuItem, Me.AdminstrationToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ChangePasswordToolStripMenuItem, Me.LogOutToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ChangePasswordToolStripMenuItem
+        '
+        Me.ChangePasswordToolStripMenuItem.Name = "ChangePasswordToolStripMenuItem"
+        Me.ChangePasswordToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ChangePasswordToolStripMenuItem.Text = "Change Password"
+        '
+        'LogOutToolStripMenuItem
+        '
+        Me.LogOutToolStripMenuItem.Name = "LogOutToolStripMenuItem"
+        Me.LogOutToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.LogOutToolStripMenuItem.Text = "Log Out"
+        '
+        'EnquiryToolStripMenuItem
+        '
+        Me.EnquiryToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CustomerCareToolStripMenuItem, Me.NewToolStripMenuItem, Me.RecordsToolStripMenuItem})
+        Me.EnquiryToolStripMenuItem.Name = "EnquiryToolStripMenuItem"
+        Me.EnquiryToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
+        Me.EnquiryToolStripMenuItem.Text = "Enquiry"
+        '
+        'CustomerCareToolStripMenuItem
+        '
+        Me.CustomerCareToolStripMenuItem.Name = "CustomerCareToolStripMenuItem"
+        Me.CustomerCareToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CustomerCareToolStripMenuItem.Text = "Customer Care"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'RecordsToolStripMenuItem
+        '
+        Me.RecordsToolStripMenuItem.Name = "RecordsToolStripMenuItem"
+        Me.RecordsToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RecordsToolStripMenuItem.Text = "Records"
+        '
+        'RecordsToolStripMenuItem1
+        '
+        Me.RecordsToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CourseListToolStripMenuItem, Me.StudentsToolStripMenuItem})
+        Me.RecordsToolStripMenuItem1.Name = "RecordsToolStripMenuItem1"
+        Me.RecordsToolStripMenuItem1.Size = New System.Drawing.Size(61, 20)
+        Me.RecordsToolStripMenuItem1.Text = "Records"
+        '
+        'CourseListToolStripMenuItem
+        '
+        Me.CourseListToolStripMenuItem.Name = "CourseListToolStripMenuItem"
+        Me.CourseListToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CourseListToolStripMenuItem.Text = "Course List"
+        '
+        'StudentsToolStripMenuItem
+        '
+        Me.StudentsToolStripMenuItem.Name = "StudentsToolStripMenuItem"
+        Me.StudentsToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.StudentsToolStripMenuItem.Text = "Students"
+        '
+        'FinanceToolStripMenuItem
+        '
+        Me.FinanceToolStripMenuItem.Name = "FinanceToolStripMenuItem"
+        Me.FinanceToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.FinanceToolStripMenuItem.Text = "Finance"
+        '
+        'UtilitiesToolStripMenuItem
+        '
+        Me.UtilitiesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RegistrationToolStripMenuItem})
+        Me.UtilitiesToolStripMenuItem.Name = "UtilitiesToolStripMenuItem"
+        Me.UtilitiesToolStripMenuItem.Size = New System.Drawing.Size(58, 20)
+        Me.UtilitiesToolStripMenuItem.Text = "Utilities"
+        '
+        'AdminstrationToolStripMenuItem
+        '
+        Me.AdminstrationToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.UsersToolStripMenuItem, Me.SettingsToolStripMenuItem})
+        Me.AdminstrationToolStripMenuItem.Name = "AdminstrationToolStripMenuItem"
+        Me.AdminstrationToolStripMenuItem.Size = New System.Drawing.Size(95, 20)
+        Me.AdminstrationToolStripMenuItem.Text = "Adminstration"
+        '
+        'RegistrationToolStripMenuItem
+        '
+        Me.RegistrationToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GradesToolStripMenuItem, Me.LevelsToolStripMenuItem, Me.ExamBodiesToolStripMenuItem, Me.CoursesToolStripMenuItem, Me.ClassesToolStripMenuItem})
+        Me.RegistrationToolStripMenuItem.Name = "RegistrationToolStripMenuItem"
+        Me.RegistrationToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RegistrationToolStripMenuItem.Text = "Registration"
+        '
+        'GradesToolStripMenuItem
+        '
+        Me.GradesToolStripMenuItem.Name = "GradesToolStripMenuItem"
+        Me.GradesToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.GradesToolStripMenuItem.Text = "Grades"
+        '
+        'LevelsToolStripMenuItem
+        '
+        Me.LevelsToolStripMenuItem.Name = "LevelsToolStripMenuItem"
+        Me.LevelsToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.LevelsToolStripMenuItem.Text = "Levels"
+        '
+        'ExamBodiesToolStripMenuItem
+        '
+        Me.ExamBodiesToolStripMenuItem.Name = "ExamBodiesToolStripMenuItem"
+        Me.ExamBodiesToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExamBodiesToolStripMenuItem.Text = "Exam Bodies"
+        '
+        'CoursesToolStripMenuItem
+        '
+        Me.CoursesToolStripMenuItem.Name = "CoursesToolStripMenuItem"
+        Me.CoursesToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CoursesToolStripMenuItem.Text = "Courses"
+        '
+        'ClassesToolStripMenuItem
+        '
+        Me.ClassesToolStripMenuItem.Name = "ClassesToolStripMenuItem"
+        Me.ClassesToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ClassesToolStripMenuItem.Text = "Classes"
+        '
+        'UsersToolStripMenuItem
+        '
+        Me.UsersToolStripMenuItem.Name = "UsersToolStripMenuItem"
+        Me.UsersToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.UsersToolStripMenuItem.Text = "Users"
+        '
+        'SettingsToolStripMenuItem
+        '
+        Me.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem"
+        Me.SettingsToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SettingsToolStripMenuItem.Text = "Settings"
+        '
+        'MDIParent
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MDIParent"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "College MIS"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ChangePasswordToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LogOutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EnquiryToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CustomerCareToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RecordsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RecordsToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents CourseListToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StudentsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FinanceToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UtilitiesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AdminstrationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RegistrationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GradesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LevelsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExamBodiesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CoursesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClassesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UsersToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SettingsToolStripMenuItem As ToolStripMenuItem
+End Class
